336e8f315b2a1d5e8a2b33f20fc4765b7921b465
Fernando Oviedo <fernando.n.oviedo@gmail.com>
2019-07-17 02:18:58 -0300

    iniciando GIT

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import WeatherLocation from './components/WeatherLocation/index';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Weather App
+      <WeatherLocation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WeatherData/WeatherExtraInfo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const WeatherExtraInfo = ({ humidity, wind })=> (
+    <div className='weatherExtraInfoCont'>
+        <span>{`${humidity} % -`}</span>
+        <span>{`${wind} wind`}</span>
+    </div>
+);
+
+WeatherExtraInfo.propTypes = {
+    humidity: PropTypes.number.isRequired,
+    wind: PropTypes.string.isRequired
+}
+
+export default WeatherExtraInfo;
